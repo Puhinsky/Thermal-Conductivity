4f4ef0b0a3506bba3270ac85bc03e6658f542871
Puhinsky <puhinsky@gmail.com>
2024-01-05 06:53:42 +0300

    Thomas implementation

--- Assets/Scripts/ForwardTask/ThomasImplictJob.cs
+++ Assets/Scripts/ForwardTask/ThomasImplictJob.cs
@@ -1,71 +1,119 @@
+using Unity.Burst;
 using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 
 namespace ForwardTask
 {
+    [BurstCompile(CompileSynchronously = true, OptimizeFor = OptimizeFor.Performance, DisableSafetyChecks = true)]
     public struct ThomasImplictJob : IJob
     {
-        public double DeltaSpace;
-        public NativeArray<double> Conductivities;
-        public NativeArray<double> Times;
-        public NativeArray<double> BoundaryConditions;
+        [ReadOnly] public double DeltaSpace;
+        [ReadOnly] public NativeArray<double> Conductivities;
+        [ReadOnly] public NativeArray<double> Times;
+        [ReadOnly] public NativeArray<double> BoundaryConditions;
         public NativeArray<double> ResultTemperatures;
+        public NativeArray<double> P;
+        public NativeArray<double> Q;
+        public NativeArray<double> Error;
 
         private int _spaceSegments;
         private int _timeSegments;
-        private NativeArray<double> _p;
-        private NativeArray<double> _q;
 
         public void Execute()
         {
             _spaceSegments = Conductivities.Length;
             _timeSegments = Times.Length;
-            _p = new NativeArray<double>(_spaceSegments, Allocator.Temp);
-            _q = new NativeArray<double>(_spaceSegments, Allocator.Temp);
+
+            CopyFirstRow();
+            CalculateOnLayer(1);
+
+            for (int i = 2; i < _timeSegments; i++)
+            {
+                CalculateOnLayer(i);
+            }
         }
 
-        private void CalculateOnZeroTime()
+        private void CalculateOnLayer(in int layer)
         {
-            var sigma = CalculateSigma(1);
-
+            var sigma = CalculateSigma(layer);
+            CalculatePQOfFirstPoint(layer);
+            CalculatePQOfOtherPoints(layer, sigma);
+            CalculateTemperatureOfLastPoint(layer);
+            BackPropagation(layer);
         }
 
-        private double CalculateSigma(int layer)
+        private double CalculateSigma(in int layer)
         {
             var deltaTime = Times[layer] - Times[layer - 1];
 
             return deltaTime / (2 * math.pow(DeltaSpace, 2));
         }
 
-        private void CalculateFirstPoint(int layer, double sigma)
+        private void CalculatePQOfFirstPoint(in int layer)
         {
-            var condCenter = Conductivities[0];
-            var condRight = Conductivities[1];
-            var b = 1 + sigma * (2 * condCenter + condRight);
-            var c = -sigma * (condCenter + condRight);
-            var d = BoundaryConditions[GetTemperatureIndex(layer, 0)];
-            _p[0] = -c / b;
-            _q[0] = d / b;
+            P[0] = 0;
+            Q[0] = BoundaryConditions[GetTemperatureIndex(layer - 1, 0)];
         }
 
-        private void CalculateMiddlePoints(int layer, double sigma)
+        private void CalculatePQOfOtherPoints(in int layer, in double sigma)
         {
             for (int i = 1; i < _spaceSegments - 1; i++)
             {
-                var condLeft = Conductivities[i-1];
+                var condLeft = Conductivities[i - 1];
                 var condCenter = Conductivities[i];
-                var condRight = Conductivities[i+1];
-                var a = sigma * ()
-                var b = 1 + sigma * (2 * condCenter + condRight);
+                var condRight = Conductivities[i + 1];
+
+                var a = -sigma * (condCenter + condLeft);
+                var b = 1 + sigma * (condLeft + 2 * condCenter + condRight);
                 var c = -sigma * (condCenter + condRight);
-                var d = BoundaryConditions[GetTemperatureIndex(layer, 0)];
+                var d = ResultTemperatures[GetTemperatureIndex(layer - 1, i)];
+
+                P[i] = -c / (a * P[i - 1] + b);
+                Q[i] = (d - a * Q[i - 1]) / (a * P[i - 1] + b);
             }
         }
 
-        private int GetTemperatureIndex(int layer, int spaceNumber)
+        private void CalculateTemperatureOfLastPoint(in int layer)
+        {
+            var lastTemperatureIndex = GetTemperatureIndex(layer, _spaceSegments - 1);
+            var lastTemperatureIndexOnPreviosLayer = GetTemperatureIndex(layer - 1, _spaceSegments - 1);
+
+            ResultTemperatures[lastTemperatureIndex] = ResultTemperatures[lastTemperatureIndexOnPreviosLayer];
+
+            EvaluateError(lastTemperatureIndex);
+        }
+
+        private void BackPropagation(in int layer)
+        {
+            for (int i = _spaceSegments - 2; i >= 0; i--)
+            {
+                var temperatureIndex = GetTemperatureIndex(layer, i);
+                ResultTemperatures[temperatureIndex] = P[i] * ResultTemperatures[temperatureIndex + 1] + Q[i];
+
+                EvaluateError(temperatureIndex);
+            }
+        }
+
+        private readonly int GetTemperatureIndex(in int layer, in int spaceNumber)
         {
             return layer * _spaceSegments + spaceNumber;
+        }
+
+        private void EvaluateError(in int index)
+        {
+            if (double.IsNegative(BoundaryConditions[index]))
+                return;
+
+            Error[0] += math.pow(ResultTemperatures[index] - BoundaryConditions[index], 2);
+        }
+
+        private void CopyFirstRow()
+        {
+            for (int i = 0; i < _spaceSegments; i++)
+            {
+                ResultTemperatures[i] = BoundaryConditions[i];
+            }
         }
     }
 }
