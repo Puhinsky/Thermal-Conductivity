abaed5c76b776817941a48798bc98bd69635f9cd
Puhinsky <puhinsky@gmail.com>
2024-01-05 06:53:42 +0300

    Thomas implementation

--- Assets/Scripts/ForwardTask/ForwardTaskRunner.cs
+++ Assets/Scripts/ForwardTask/ForwardTaskRunner.cs
@@ -1,4 +1,8 @@
 using DatabaseLibrary;
+using System.Diagnostics;
+using Unity.Collections;
+using Unity.Jobs;
+using Debug = UnityEngine.Debug;
 
 namespace ForwardTask
 {
@@ -6,7 +10,40 @@ namespace ForwardTask
     {
         public void Execute(Database database)
         {
+            var conductivities = new NativeArray<double>(database.Conductivities, Allocator.Persistent);
+            var times = new NativeArray<double>(database.Times, Allocator.Persistent);
+            var boundaryConditions = new NativeArray<double>(database.Temperatures, Allocator.Persistent);
+            var result = new NativeArray<double>(database.SpaceSegmentsCount * database.TimeSegmentsCount, Allocator.Persistent);
+            var p = new NativeArray<double>(database.SpaceSegmentsCount - 1, Allocator.Persistent);
+            var q = new NativeArray<double>(database.SpaceSegmentsCount - 1, Allocator.Persistent);
+            var error = new NativeArray<double>(1, Allocator.Persistent);
 
+            var deltaSpace = database.Lenght / database.SpaceSegmentsCount;
+
+            var thomasJob = new ThomasImplictJob()
+            {
+                DeltaSpace = deltaSpace,
+                Conductivities = conductivities,
+                Times = times,
+                BoundaryConditions = boundaryConditions,
+                ResultTemperatures = result,
+                P = p,
+                Q = q,
+                Error = error
+            };
+
+            var watch = Stopwatch.StartNew();
+            var handle = thomasJob.Schedule();
+            handle.Complete();
+            watch.Stop();
+            Debug.Log($"Forward task on CPU executed in {watch.ElapsedMilliseconds} ms. Error is {error[0]}");
+
+            conductivities.Dispose();
+            times.Dispose();
+            boundaryConditions.Dispose();
+            result.Dispose();
+            p.Dispose();
+            q.Dispose();
         }
     }
 }
